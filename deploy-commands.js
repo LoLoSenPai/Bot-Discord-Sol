11c6ed09abf2c4e4073917a81dc2315f721fa756
LoLoSenPai <loic.dlugosz@gmail.com>
2022-10-22 18:41:31 +0200

    suite5

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { REST, Routes } = require('discord.js');
 const { token, clientId, guildId} = require('./config.json');
 
