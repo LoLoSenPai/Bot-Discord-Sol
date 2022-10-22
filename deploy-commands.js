bef674ef6ab0ba13216cdcfd75aa59cd70f2d9c9
LoLoSenPai <loic.dlugosz@gmail.com>
2022-10-22 17:24:48 +0200

    maj mineur
    remplacement require('node:fs') par ('fs') car problèmes de compatibilité avec la V18 de node

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs');
+const fs = require('fs');
 const path = require('node:path');
 const { REST, Routes } = require('discord.js');
 const { token, clientId, guildId} = require('./config.json');
