f99e5c64bb04e5598abb425388e195904afc53cd
LoLoSenPai <loic.dlugosz@gmail.com>
2022-11-12 20:01:44 +0100

    maj
    Ajout commande poll + clear

---
+++ commands/clear.js
@@ -0,0 +1,56 @@
+const {SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Supprime un montant spécifique depuis un channel ciblé.")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addIntegerOption(option =>
+        option.setName('amount')
+        .setDescription('Nombre de messages à supprimer.')
+        .setMinValue(1)
+        .setMaxValue(99)
+        .setRequired(true)
+        )
+    .addUserOption(option =>
+        option.setName('target')
+        .setDescription('Selectionner un channel.')
+        .setRequired(false)
+        ),
+
+    async execute(interaction) {
+        const {channel, options} = interaction;
+
+        const amount = options.getInteger('amount');
+        const target = options.getUser("target");
+
+        const messages = await channel.messages.fetch({
+            limit: amount +1,
+        });
+
+        const res = new EmbedBuilder()
+            .setColor(0x5fb041)
+
+        if(target) {
+            let i = 0;
+            const filtered = [];
+
+            (await messages).filter((msg) =>{
+                if(msg.author.id === target.id && amount > i) {
+                    filtered.push(msg);
+                    i++;
+                }
+            });
+
+            await channel.bulkDelete(filtered).then(messages => {
+                res.setDescription(`Succesfully deleted ${messages.size} messages from ${target}.`);
+                interaction.reply({embeds: [res]}); // you can use ephemeral if you desire
+            });
+        } else {
+            await channel.bulkDelete(amount, true).then(messages => {
+                res.setDescription(`Succesfully deleted ${messages.size} messages from the channel.`);
+                interaction.reply({embeds: [res]});
+            });
+        }
+    }
+}
---
+++ commands/poll.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("poll")
+        .setDescription("Créer un sondage et l'envoyer dans un channel")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addStringOption(option =>
+            option.setName("description")
+                .setDescription("Décris le sondage.")
+                .setRequired(true)
+        )
+        .addChannelOption(option =>
+            option.setName("channel")
+                .setDescription("Dans quel salon veux-tu l'envoyer?")
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText)
+        ),
+    async execute(interaction) {
+        const { options } = interaction;
+
+        const channel = options.getChannel("channel");
+        const description = options.getString("description");
+
+        const embed = new EmbedBuilder()
+            .setColor("Gold")
+            .setDescription(description)
+            .setTimestamp();
+
+        try {
+            const m = await channel.send({ embeds: [embed] });
+            await m.react("✅");
+            await m.react("❌");
+            await interaction.reply({ content: "Le sondage a été envoyé avec succès.", ephemeral: true });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
